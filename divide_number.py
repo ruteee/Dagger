a2663d2ae55cd3488cb5edde8b4f647e163752ea
Rute Abreu <rute.s.abreu@gmail.com>
2022-08-28 15:14:05 -0300

    Addition of functions of the workflow_test0

---
+++ divide_number.py
@@ -0,0 +1,5 @@
+def main(previous_results, divisor):
+    '''
+     Receives te result of the previous task (0) (sum of 'a' and 'b') and divides by 'divisor' parameter)
+    '''
+    return previous_results['task_0']/divisor
---
+++ sum_numbers.py
@@ -0,0 +1,5 @@
+def main(a, b):
+    '''
+     Sum the two number a and b
+    '''
+    return a + b
